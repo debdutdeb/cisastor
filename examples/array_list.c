21d5e75873ce055929dae1692d1cb07f41e7defb
Debdut <debdutdeb@outlook.com>
2026-04-28 12:45:37 +0530

    chore(array_list): type specific macro expander

--- examples/array_list.c
+++ examples/array_list.c
@@ -2,22 +2,31 @@
 #include "macros.h"
 
 #include <assert.h>
+#include <stdio.h>
 
-#define array_list_get_int_at(a, b) *cast(int *, array_list_get_element_at(a, b))
+array_list_init(int);
 
-int *array_list_append_int(struct array_list *al, int num) {
-  return cast(int *, array_list_append(al, &num));
-}
+struct foo {
+  int num;
+};
+
+typedef struct foo foo;
+
+array_list_init(foo);
 
 int main() {
-  struct array_list *al = array_list_create(sizeof(int));
+  struct array_list *al = array_list_create_int();
   assert(null != al);
-  assert(null != array_list_append_int(al, 1));
-  assert(null != array_list_append_int(al, 3));
-  assert(null != array_list_append_int(al, 4));
-  assert(null != array_list_append_int(al, 5));
-  assert(null != array_list_append_int(al, 6));
-  assert(null != array_list_append_int(al, 7));
-  assert(null != array_list_append_int(al, 8));
-  assert(6 == array_list_get_int_at(al, 4));
+  array_list_append_int(al, 1);
+  array_list_append_int(al, 2);
+  array_list_append_int(al, 3);
+  assert(3 == array_list_get_int_at(al, 2));
+
+  struct array_list *al2 = array_list_create_foo();
+  assert(null != al2);
+  struct foo f = {.num = 1};
+  array_list_append_foo(al2, f);
+  f.num = 2;
+  array_list_append_foo(al2, f);
+  printf("at 1 %d\n", array_list_get_foo_at(al2, 1).num);
 }
