b17652a39e154c978bb88acf89f744aac1699d32
Debdut <debdutdeb@outlook.com>
2026-05-04 10:35:06 +0530

    fix(progressbar): use platform specific types for cookie functions

--- libs/progressbar/progressbar.c
+++ libs/progressbar/progressbar.c
@@ -7,10 +7,10 @@
 #include "macros.h"
 
 #include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -23,7 +23,7 @@ typedef struct progress_bar_options {
 typedef struct progress_bar {
   ProgressBarOptions *options;
 
-  FILE* dest;
+  FILE *dest;
 
   int written;
   float percent;
@@ -44,7 +44,7 @@ char *progressbar_build(struct progress_bar *const pb, int n) {
   int fill = floor(floor(pb->bar_width) * pb->percent / 100.00);
 
   for (int i = pb->columns_written; i < fill; i++) {
-	  pb->bar[i] = '=';
+    pb->bar[i] = '=';
   }
 
   pb->columns_written = fill;
@@ -52,27 +52,39 @@ char *progressbar_build(struct progress_bar *const pb, int n) {
   return pb->bar;
 }
 
-static int __progressbar_write(void *cookie, const char *buf, int n) {
+#if defined(__APPLE__) && defined(__MACH__)
+typedef int pb_write_ret_t;
+typedef int pb_write_len_t;
+#elif defined(__linux__)
+typedef long pb_write_ret_t;
+typedef unsigned long pb_write_len_t;
+#else
+#error "Only macOS and Linux are supported"
+#endif
+
+static pb_write_ret_t __progressbar_write(void *cookie, const char *buf,
+                                          pb_write_len_t n) {
   ProgressBar *pb = cast(ProgressBar *, cookie);
 
   if (0 == n) {
-	  return fwrite(buf, 1, 0, pb->dest);
+    return fwrite(buf, 1, 0, pb->dest);
   }
 
-  fprintf(stderr, "\r[%s] [%s] %.2f%%", pb->options->label, progressbar_build(pb, n), pb->percent);
+  fprintf(stderr, "\r[%s] [%s] %.2f%%", pb->options->label,
+          progressbar_build(pb, n), pb->percent);
   fflush(stderr);
 
   return fwrite(buf, 1, n, pb->dest);
 }
 
 static int __progressbar_close(void *cookie) {
-    ProgressBar *pb = cast(ProgressBar *, cookie);
-    fprintf(stderr, "\n");
-    fflush(stderr);
-    int ret = fclose(pb->dest);
-    free(pb->bar);
-    free(pb);
-    return ret;
+  ProgressBar *pb = cast(ProgressBar *, cookie);
+  fprintf(stderr, "\n");
+  fflush(stderr);
+  int ret = fclose(pb->dest);
+  free(pb->bar);
+  free(pb);
+  return ret;
 }
 
 FILE *progressbar_open(FILE *fp, ProgressBarOptions *options) {
@@ -80,7 +92,7 @@ FILE *progressbar_open(FILE *fp, ProgressBarOptions *options) {
     return null;
   }
 
-  ProgressBar *pb = cast(ProgressBar *, calloc(1,sizeof(struct progress_bar)));
+  ProgressBar *pb = cast(ProgressBar *, calloc(1, sizeof(struct progress_bar)));
   if (null == pb) {
     errno = ENOMEM;
     return null;
@@ -90,18 +102,19 @@ FILE *progressbar_open(FILE *fp, ProgressBarOptions *options) {
 
   struct winsize win;
   if (-1 == ioctl(STDERR_FILENO, TIOCGWINSZ, &win)) {
-	  win.ws_col = 80; // fallback
+    win.ws_col = 80; // fallback
   }
 
-  pb->bar_width = (int)win.ws_col - (int)strlen(options->label) - extra_character_length;
+  pb->bar_width =
+      (int)win.ws_col - (int)strlen(options->label) - extra_character_length;
   if (pb->bar_width < 1) {
-	  pb->bar_width = 1;
+    pb->bar_width = 1;
   }
 
-  pb->bar = cast(char*, malloc(sizeof(char)*pb->bar_width + 1));
+  pb->bar = cast(char *, malloc(sizeof(char) * pb->bar_width + 1));
   if (null == pb->bar) {
-	  free(pb);
-	  return null;
+    free(pb);
+    return null;
   }
 
   memset(pb->bar, ' ', pb->bar_width);
@@ -110,9 +123,10 @@ FILE *progressbar_open(FILE *fp, ProgressBarOptions *options) {
   pb->dest = fp;
 
 #ifdef __APPLE__
-    return funopen(pb, NULL, __progressbar_write, NULL, __progressbar_close);
+  return funopen(pb, NULL, __progressbar_write, NULL, __progressbar_close);
 #else
-    cookie_io_functions_t io = { .write = __progressbar_write, .close = __progressbar_close };
-    return fopencookie(pb, "w", io);
+  cookie_io_functions_t io = {.write = __progressbar_write,
+                              .close = __progressbar_close};
+  return fopencookie(pb, "w", io);
 #endif
 }
